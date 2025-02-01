1a48dccb875e3e5c26150e046426c3904bfe1fd0
Cagri Yildiz <cagriyild@gmail.com>
2025-02-01 15:26:54 +0100

    Add CRUD for reports

---
+++ db/store/reports.go
@@ -0,0 +1,89 @@
+package store
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/jmoiron/sqlx"
+	"time"
+)
+
+type ReportsStore struct {
+	db *sqlx.DB
+}
+
+func NewReportsStore(db *sql.DB) *ReportsStore {
+	return &ReportsStore{
+		db: sqlx.NewDb(db, "postgres"),
+	}
+}
+
+type Report struct {
+	UserId               uuid.UUID  `db:"user_id"`
+	Id                   uuid.UUID  `db:"id"`
+	ReportType           string     `db:"report_type"`
+	OutputFilePath       *string    `db:"output_file_path"`
+	DownloadUrl          *string    `db:"download_url"`
+	DownloadUrlExpiresAt *time.Time `db:"download_url_expires_at"`
+	ErrorMessage         *string    `db:"error_message"`
+	CreatedAt            time.Time  `db:"created_at"`
+	StartedAt            *time.Time `db:"started_at"`
+	FailedAt             *time.Time `db:"failed_at"`
+	CompletedAt          *time.Time `db:"completed_at"`
+}
+
+func (s *ReportsStore) CreateReport(ctx context.Context, userId uuid.UUID, reportType string) (*Report, error) {
+	const query = `INSERT INTO reports (user_id, report_type) VALUES ($1, $2) RETURNING *`
+
+	var report Report
+	err := s.db.GetContext(ctx, &report, query, userId, reportType)
+	if err != nil {
+		return nil, fmt.Errorf("failed to insert report: %w", err)
+	}
+
+	return &report, nil
+}
+
+func (s *ReportsStore) UpdateReport(ctx context.Context, report *Report) (*Report, error) {
+	const query = `UPDATE reports 
+				   SET report_type = $1,
+				       output_file_path = $2, 
+				       download_url = $3, 
+				       download_url_expires_at = $4, 
+				       error_message = $5, 
+				       started_at = $6, 
+				       failed_at = $7, 
+				       completed_at = $8 
+				   WHERE user_id = $9 and id = $10 RETURNING *`
+
+	var updatedReport Report
+	err := s.db.GetContext(ctx, &updatedReport, query,
+		report.ReportType,
+		report.OutputFilePath,
+		report.DownloadUrl,
+		report.DownloadUrlExpiresAt,
+		report.ErrorMessage,
+		report.StartedAt,
+		report.FailedAt,
+		report.CompletedAt,
+		report.UserId,
+		report.Id,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to update report: %w", err)
+	}
+
+	return &updatedReport, nil
+}
+
+func (s *ReportsStore) GetReportByPrimaryKey(ctx context.Context, userId, id uuid.UUID) (*Report, error) {
+	const query = `SELECT * FROM reports WHERE user_id = $1 AND id = $2`
+
+	var report Report
+	err := s.db.GetContext(ctx, &report, query, userId, id)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get report: %w", err)
+	}
+	return &report, nil
+}
---
+++ db/store/reports_test.go
@@ -0,0 +1,56 @@
+package store
+
+import (
+	"context"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+func TestReportsStore(t *testing.T) {
+	testDB := NewTestDB(t)
+	cleanup := testDB.Setup(t)
+	t.Cleanup(func() {
+		cleanup(t)
+	})
+
+	now := time.Now()
+	ctx := context.Background()
+
+	userStore := NewUserStore(testDB.DB)
+	user, err := userStore.CreateUser(ctx, "test@test.com", "test")
+	require.NoError(t, err)
+
+	reportsStore := NewReportsStore(testDB.DB)
+	report, err := reportsStore.CreateReport(ctx, user.Id, "test")
+	require.NoError(t, err)
+
+	require.Equal(t, user.Id, report.UserId)
+	require.Equal(t, "test", report.ReportType)
+	require.True(t, report.CreatedAt.After(now))
+
+	startedAt := report.CreatedAt.Add(time.Second)
+	completedAt := report.CreatedAt.Add(2 * time.Second)
+	failedAt := report.CreatedAt.Add(3 * time.Second)
+	errMsg := "error"
+	downloadUrl := "http://localhost:8080/reports"
+	outputPath := "s3://reports-test/reports"
+	downloadUrlExpiresAt := report.CreatedAt.Add(4 * time.Second)
+
+	report.ReportType = "ex"
+	report.OutputFilePath = &outputPath
+	report.DownloadUrl = &downloadUrl
+	report.DownloadUrlExpiresAt = &downloadUrlExpiresAt
+	report.StartedAt = &startedAt
+	report.CompletedAt = &completedAt
+	report.FailedAt = &failedAt
+	report.ErrorMessage = &errMsg
+
+	updatedRecord, err := reportsStore.UpdateReport(ctx, report)
+	require.NoError(t, err)
+	require.Equal(t, report, updatedRecord)
+
+	gotReport, err := reportsStore.GetReportByPrimaryKey(ctx, report.UserId, report.Id)
+	require.NoError(t, err)
+	require.Equal(t, updatedRecord, gotReport)
+}
